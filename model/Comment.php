bcfb27b0707a3dad933eed7eacb7aac565c243dc
kholoud001 <kholoud.sanak@gmail.com>
2023-12-19 10:16:11 +0100

    comment class update

--- model/Comment.php
+++ model/Comment.php
@@ -22,4 +22,28 @@ class Comment {
         $this->db->query($query);
         $this->db->execute();
     }
+    /////////////////////////////////////  Display comment  ///////////////////////////////////////////////
+    function displayComments()
+    {
+        $sql = "SELECT comment.*, user.* FROM comment
+                INNER JOIN user ON comment.user_id=user.user_id
+                WHERE ticket_id=?";
+        $this->db->query($sql);
+        $this->db->bind(1, $this->ticket_id, PDO::PARAM_INT);
+        $this->db->execute();
+        $comments = $this->db->resultSet();
+        return $comments;
+    }
+    /////////////////////////////////////  Create comment  ///////////////////////////////////////////////
+
+    function createComment()
+    {
+        $sql = "INSERT INTO comment (comment, ticket_id, user_id) VALUES (?, ?, ?)";
+        $this->db->query($sql);
+        $this->db->bind(1, $this->comment, PDO::PARAM_STR);
+        $this->db->bind(2, $this->ticket_id, PDO::PARAM_INT);
+        $this->db->bind(3, $this->comment_user_id, PDO::PARAM_INT);
+        $this->db->execute();
+    }
+
 }
