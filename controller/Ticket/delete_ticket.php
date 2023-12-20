ac0d681baf9e6de7cde42a90286e684f13c3b8c9
kholoud001 <kholoud.sanak@gmail.com>
2023-12-20 14:18:47 +0100

    delete ticket method

--- controller/Ticket/delete_ticket.php
+++ controller/Ticket/delete_ticket.php
@@ -5,14 +5,14 @@ require_once "../../model/Ticket.php";
 // Check if the ticket ID is provided in the URL
 if (isset($_POST['id'])) {
     $ticketId = $_POST['id'];
+    echo "Ticket ID: $ticketId";
 
     // Create an instance of the Ticket class
     $ticketModel = new Ticket();
 
-    // Attempt to delete the ticket
-    $success = $ticketModel->deleteTicket($ticketId);
+    $rowCount = $ticketModel->deleteTicket($ticketId);
 
-    if ($success) {
+    if ($rowCount > 0) {
         header("Location: display_tickets.php");
         exit();
     } else {
